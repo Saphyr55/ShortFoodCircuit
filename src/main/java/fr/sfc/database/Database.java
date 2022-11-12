f9d91f3bfdc9cb7cb07c58ab92ff14fa2fe0d87f
Saphyr55 <saphyr0550@gmail.com>
2022-11-12 11:14:55 +0100

    add database class and default.fxml

--- src/main/java/fr/sfc/database/Database.java
+++ src/main/java/fr/sfc/database/Database.java
@@ -1,22 +1,72 @@
 package fr.sfc.database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 
-public class Database {
+public class Database implements AutoCloseable {
 
-	private static Database database = null;
- 	private final static HashMap<String, Connection> connections = new HashMap<>(10);
+	private static final Map<String, Database> databases = new HashMap<>();
+	private static final String mainDatabaseName = "sfc";
+	private static String currentDbName = mainDatabaseName;
+	private static final String CLASS_DRIVER = "com.mysql.cj.jdbc.Driver";
+	static { initDriver(); }
+
+	private final String ip = "127.0.0.1";
+	private final String password = "";
+	private final String user = "root";
+	private final String port = "3306";
+	private Connection connection;
+	private String SGDBName;
 	private String url;
-	private String password;
-	private String user;
-	private String port;
-	private String dbName;
 
-	public Database() {
-
+	public Database(final String dbName) throws SQLException {
+		SGDBName = CLASS_DRIVER.split("\\.")[1];
+		url = "jdbc:" + SGDBName + "://" + ip + ":" + port + "/" + dbName;
+		connection = DriverManager.getConnection(url, user, password);
 	}
 
+	public static void switchDB(String dbname) {
+		currentDbName = dbname;
+	}
+
+	public static void switchMainDB() {
+		currentDbName = mainDatabaseName;
+	}
+
+	public static Database get() {
+		if (!databases.containsKey(currentDbName)) {
+			try {
+				databases.put(currentDbName, new Database(currentDbName));
+			} catch (SQLException e) {
+				e.printStackTrace();
+				switchMainDB();
+			}
+		}
+		return databases.get(currentDbName);
+	}
+
+	private static void initDriver() {
+		try {
+			Class.forName(CLASS_DRIVER);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public static String getCurrentDbName() {
+		return currentDbName;
+	}
+
+	@Override
+	public void close() throws Exception {
+		connection.close();
+	}
 
 }
