5cb9aee5baa3ba28842350aa7e2108416d7f5e90
Saphyr55 <saphyr0550@gmail.com>
2022-11-27 19:55:40 +0100

    petite optimization sur injection

--- src/main/java/fr/sfc/api/persistence/InjectConfiguration.java
+++ src/main/java/fr/sfc/api/persistence/InjectConfiguration.java
@@ -1,6 +1,7 @@
 package fr.sfc.api.persistence;
 
 import com.google.common.collect.Sets;
+import fr.sfc.api.component.Component;
 import fr.sfc.api.component.ComponentFactory;
 import fr.sfc.api.controller.ControllerFactory;
 import fr.sfc.api.persistence.annotation.Inject;
@@ -36,37 +37,36 @@ public final class InjectConfiguration {
     }
     
     private void configureForRepository() {
-        repositoryFactory.getAllRepository().forEach(repository -> Arrays
+        repositoryFactory.getAllRepository().forEach((final var repository) -> Arrays
                         .stream(repository.getClass().getDeclaredFields())
                         .filter(this::haveInjectAnnotation)
                         .forEach(field -> setValueFieldToAll(field, repository)));
     }
 
     private void configureForComponent() {
-        componentFactory.getAllComponents().forEach(component -> Arrays
+        componentFactory.getAllComponents().forEach((final var component) -> Arrays
                         .stream(component.getClass().getDeclaredFields())
                         .filter(this::haveInjectAnnotation)
                         .forEach(field -> setValueFieldToAll(field, component)));
     }
 
     private void configureForController() {
-        controllerFactory.getAllControllers().forEach(controller -> Arrays
+        controllerFactory.getAllControllers().forEach((final var controller) -> Arrays
                         .stream(controller.getClass().getDeclaredFields())
                         .filter(this::haveInjectAnnotation)
                         .forEach(field -> setValueFieldToAll(field, controller)));
     }
 
-    private void setValueFieldToAll(Field field, Object instance) {
-        field.getType();
+    private void setValueFieldToAll(final Field field, final Object instance) {
         setValueFieldToEntityManager(field, instance);
         setValueFieldToRepository(field, instance);
     }
 
-    private boolean haveInjectAnnotation(Field field) {
+    private boolean haveInjectAnnotation(final Field field) {
         return field.getAnnotation(Inject.class) != null;
     }
 
-    private void setValueFieldToRepository(Field field, Object instance) {
+    private void setValueFieldToRepository(final Field field, final Object instance) {
         final List<? extends Repository<?>> repositories = repositoryFactory.getAllRepository();
         for (final var repository : repositories)
             setFieldValueForObject(repository.getClass(), field, instance, repository);
@@ -84,7 +84,7 @@ public final class InjectConfiguration {
         }
     }
 
-    private void setValueFieldToEntityManager(Field field, Object instance) {
+    private void setValueFieldToEntityManager(final Field field, final Object instance) {
         setFieldValueForObject(EntityManager.class, field, instance, entityManager);
     }
 
