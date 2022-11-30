22d957aef7dad7702cd7b7244e5267d0b2d11efc
Saphyr55 <saphyr0550@gmail.com>
2022-11-30 09:49:31 +0100

    remove test

--- src/test/java/fr/sfc/SFCApplicationTest.java
+++ src/test/java/fr/sfc/SFCApplicationTest.java
@@ -14,26 +14,5 @@ import static org.testfx.framework.junit5.ApplicationTest.launch;
 @ExtendWith(ApplicationExtension.class)
 class SFCApplicationTest {
 
-    SFCApplication app;
-
-    Button button;
-
-    @BeforeAll
-    static void setup() throws Exception {
-        launch(SFCApplication.class);
-    }
-
-    @Start
-    public void start(Stage stage) throws Exception {
-        app = new SFCApplication();
-        app.start(stage);
-        button = new Button("clicked");
-    }
-
-    @Test
-    void buttonWork() {
-        Assertions.assertThat(button).hasText("clicked!");
-    }
-
 
 }
