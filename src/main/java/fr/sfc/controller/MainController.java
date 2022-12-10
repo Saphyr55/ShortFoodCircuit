81f7b2e9d6407c4c5bdc980d2fee531fede138ad
Saphyr55 <saphyr0550@gmail.com>
2022-12-10 08:59:05 +0100

    finish tag system

--- src/main/java/fr/sfc/controller/MainController.java
+++ src/main/java/fr/sfc/controller/MainController.java
@@ -54,4 +54,6 @@ public class MainController implements Controller {
     }
 
 
+
+
 }
--- src/main/java/fr/sfc/controller/productTour/MapController.java
+++ src/main/java/fr/sfc/controller/productTour/MapController.java
@@ -1,20 +1,63 @@
 package fr.sfc.controller.productTour;
 
-import fr.sfc.framework.Resources;
-import fr.sfc.framework.controlling.annotation.AutoComponent;
-import fr.sfc.framework.controlling.Controller;
 import fr.sfc.component.productTour.MapComponent;
+import fr.sfc.framework.Resources;
+import fr.sfc.framework.controlling.Controller;
+import fr.sfc.framework.controlling.annotation.AutoComponent;
+import javafx.concurrent.Worker;
 import javafx.scene.web.WebEngine;
+import netscape.javascript.JSObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class MapController implements Controller {
     
     @AutoComponent
     private MapComponent component;
 
+    private WebEngine engine;
+    private JSObject js;
+    private JavaConnector javaConnector = new JavaConnector();
+
     @Override
     public void setup() {
-        final WebEngine engine = component.getWwMap().getEngine();
-        engine.load(Resources.getResource("/html/index.html").toExternalForm());
+        engine = component.getWwMap().getEngine();
+        engine.load(Resources.getResource("/html/map.html").toExternalForm());
+
+        //loadJsFile("/js/map.js");
+
+        engine.getLoadWorker()
+                .stateProperty()
+                .addListener((obs, old, state) -> loadJavascript(engine, state));
+        engine.reload();
+    }
+
+    private void loadJavascript(WebEngine engine, Worker.State state) {
+        if (state == Worker.State.SUCCEEDED) {
+            double latitude = 48.866667;
+            double longitude = 2.333333;
+            js = (JSObject) engine.executeScript("window");
+            js.setMember("javac", javaConnector);
+            Object map = js.call("createMap",  latitude, longitude);
+        }
+    }
+
+    private void loadJsFile(String resource) {
+        try {
+            var content = Files.readString(Resources.getFileResource(resource).toPath());
+            engine.loadContent("<script type=\"text/javascript\">"+content+"</script>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class JavaConnector {
+
+        public double connect() {
+            return 20.3;
+        }
+
     }
 
 }
