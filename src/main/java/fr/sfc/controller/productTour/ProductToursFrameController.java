62af86922fec9bd31a4096b66795d81038df531b
v <116445593+beieb@users.noreply.github.com>
2022-12-13 15:07:30 +0100

    V (#17)
    * creat new ProductTour when Button Finish is use
    
    * insert prodTours in BDD

--- src/main/java/fr/sfc/controller/productTour/ProductToursFrameController.java
+++ src/main/java/fr/sfc/controller/productTour/ProductToursFrameController.java
@@ -1,14 +1,17 @@
 package fr.sfc.controller.productTour;
 
 import fr.sfc.container.productTour.ProductTourFrame;
+import fr.sfc.entity.ProductTour;
 import fr.sfc.framework.controlling.Controller;
 import fr.sfc.framework.controlling.annotation.AutoContainer;
 import fr.sfc.framework.persistence.annotation.Inject;
 import fr.sfc.repository.ProductTourRepository;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.stream.Collectors;
 
@@ -16,6 +19,21 @@ public class ProductToursFrameController implements Controller {
 
     @FXML
     private TextField tFName;
+    @FXML
+    private DatePicker startDate;
+    @FXML
+    private DatePicker endDate;
+    @FXML
+    private TextField tFWeight;
+    @FXML
+    private TextField tFIdCompany;
+    @FXML
+    private TextField tfSIRETCompany;
+    @FXML
+    private TextField tFVehicleId;
+    @FXML
+    private TextField tFMatriculation;
+
 
     @AutoContainer
     private ProductTourFrame container;
@@ -31,7 +49,22 @@ public class ProductToursFrameController implements Controller {
 
     @FXML
     public void EventButtonAddProductToursFinishAction() {
-        System.out.println(tFName);
+        LocalDateTime localStartDate = this.startDate.getValue().atStartOfDay();
+        LocalDateTime localEndDate = this.endDate.getValue().atStartOfDay();
+        Float weight = Float.valueOf(tFWeight.getText());
+        Integer idCompany = Integer.parseInt(this.tFIdCompany.getText());
+        Integer SIRET = Integer.parseInt(this.tfSIRETCompany.getText());
+        Integer idVehicle = Integer.valueOf(this.tFVehicleId.getText());
+        ProductTour newprodTour = new ProductTour(localStartDate,
+                localEndDate,
+                this.tFName.getText(),
+                weight,
+                idCompany,
+                SIRET,
+                idVehicle,
+                this.tFMatriculation.getText());
+        productTourRepository.insert(newprodTour);
+
     }
 
 }
