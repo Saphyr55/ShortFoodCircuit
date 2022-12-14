734482ce4ee3ba16d8818d921d41ec06f39ce1c8
Saphyr55 <saphyr0550@gmail.com>
2022-12-14 18:45:33 +0100

    fix build

--- src/main/java/fr/sfc/container/admin/ListProducerContainer.java
+++ src/main/java/fr/sfc/container/admin/ListProducerContainer.java
@@ -1,7 +1,5 @@
 package fr.sfc.container.admin;
 
-import javax.swing.text.html.ListView;
-
 import fr.sfc.controller.admin.ListProducerController;
 import fr.sfc.framework.controlling.Container;
 import fr.sfc.framework.controlling.annotation.AutoController;
@@ -11,6 +9,7 @@ import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
