56cbf5bf4820fd54829cb7dce7c1f222a47e7c02
Saphyr55 <saphyr0550@gmail.com>
2022-12-15 10:17:39 +0100

    update window admin

--- src/main/java/fr/sfc/container/productTour/ConfigProductTourContainer.java
+++ src/main/java/fr/sfc/container/productTour/ConfigProductTourContainer.java
@@ -1,13 +1,30 @@
 package fr.sfc.container.productTour;
 
+import fr.sfc.framework.Resources;
 import fr.sfc.framework.controlling.Container;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class ConfigProductTourContainer extends AnchorPane implements Container {
 
 
-    @Override
-    public void setup() {
+    private FXMLLoader loader;
 
+    public ConfigProductTourContainer() throws IOException {
+        loader = new FXMLLoader();
+        loader.load(Resources.getResource("/fxml/specifiesProductTour.fxml"));
+        loader.setRoot(this);
+    }
+
+    @Override
+    public void setup()  {
+
+    }
+
+    @Override
+    public FXMLLoader getLoader() {
+        return loader;
     }
 }
