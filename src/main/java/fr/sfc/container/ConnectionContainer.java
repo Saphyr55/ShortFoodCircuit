686fdf49ce86e10efd18f1f5f92b59359f50ac3e
Loupiio <matt.moumou@hotmail.fr>
2022-12-18 23:42:05 +0100

    Ajout de la page de connexion

--- src/main/java/fr/sfc/container/ConnectionContainer.java
+++ src/main/java/fr/sfc/container/ConnectionContainer.java
@@ -1,12 +1,48 @@
 package fr.sfc.container;
 
+import fr.sfc.container.admin.MainAdminContainer;
+import fr.sfc.controller.ConnectionController;
+import fr.sfc.framework.Resources;
 import fr.sfc.framework.controlling.Container;
+import fr.sfc.framework.controlling.annotation.AutoController;
+import fr.sfc.framework.controlling.annotation.ContainerFXML;
+import fr.sfc.framework.controlling.annotation.SetContainer;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import java.io.IOException;
 
-public class ConnectionContainer implements Container {
+@ContainerFXML
+public class ConnectionContainer extends GridPane implements Container {
 
+    @AutoController
+    private ConnectionController controller;
+
+    private final FXMLLoader loader;
+
+    @SetContainer
+    private MainAdminContainer mainAdminContainer;
+
+
+    public ConnectionContainer() {
+        loader = new FXMLLoader(Resources.getResource("/fxml/connection.fxml"));
+        try {
+            loader.setRoot(this);
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void setup() {
 
     }
+
+    @Override
+    public FXMLLoader getLoader() {
+        return loader;
+    }
+
 }
---
+++ src/main/java/fr/sfc/controller/ConnectionController.java
@@ -0,0 +1,88 @@
+package fr.sfc.controller;
+
+import fr.sfc.container.ConnectionContainer;
+import fr.sfc.entity.Admin;
+import fr.sfc.entity.Producer;
+import fr.sfc.framework.BackendApplication;
+import fr.sfc.framework.controlling.ContainerManager;
+import fr.sfc.framework.controlling.Controller;
+import fr.sfc.framework.controlling.annotation.AutoContainer;
+import fr.sfc.framework.persistence.annotation.Inject;
+import fr.sfc.repository.AdminRepository;
+import fr.sfc.repository.ProducerRepository;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ConnectionController implements Controller {
+
+    @FXML
+    public PasswordField passwordTextField;
+    @FXML
+    public TextField idTextField;
+    @AutoContainer
+    private ConnectionContainer container;
+
+    @Inject
+    private ContainerManager containerManager;
+
+    @Inject
+    private ProducerRepository producerRepository;
+
+    @Inject
+    private AdminRepository adminRepository;
+
+    @FXML
+    public void onPressLogin(){
+        var entity = getEntity(getDataIdTextField());
+        if(entity instanceof Admin admin && admin.getPassword().equals(passwordTextField.getText())){
+            BackendApplication.getCurrentApplication().getPrimaryStage().hide();
+            System.out.println(passwordTextField.getText());
+        } else if (entity instanceof Producer producer && producer.getPassword().equals(passwordTextField.getText()) ){
+            BackendApplication.getCurrentApplication().getPrimaryStage().hide();
+        }
+
+    }
+
+    public String getDataIdTextField(){
+        return idTextField.getText();
+    }
+
+    public int getIdIntegerFromStringId(String id){
+        char[] chars = id.toCharArray();
+        String cStream = IntStream.range(0, chars.length).filter(i -> i != chars.length-1).mapToObj(i -> chars[i]).map(String::valueOf).collect(Collectors.joining());
+        try{
+            return Integer.parseInt(cStream);
+        }catch (Exception e){
+            return -1;
+        }
+    }
+
+    public <E> E getEntity(String id){
+        switch ( id.charAt(id.length() - 1) ){
+            case 'a' ->{
+                int idValue = getIdIntegerFromStringId(id);
+                if (idValue < 0)
+                    return null;
+                return (E)adminRepository.find(idValue);
+            }
+            case 'p' -> {
+                int idValue = getIdIntegerFromStringId(id);
+                if (idValue < 0)
+                    return null;
+                return (E)producerRepository.find(idValue);
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public void setup() {
+        var producers = producerRepository.findAll();
+    }
+}
